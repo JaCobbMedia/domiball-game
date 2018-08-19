f212258a139e0ae2443dece958353627dc982f3d
JaCobbMedia <joksavodnikas@gmail.com>
2018-08-19 15:05:40 +0300

    Simple Ball and Cam movement
    Implemented basic ball and camera movement

---
+++ DomiBall/Assets/Scripts/BallControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rigidbody;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+    private void FixedUpdate()
+    {
+        var moveHori = Input.GetAxis("Horizontal");
+        var moveVert = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHori, 0.0f, moveVert);
+
+        rigidbody.AddForce(movement * speed);
+    }
+}
---
+++ DomiBall/Assets/Scripts/CamControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour {
+
+    public Transform target;
+
+    public float camSpeed;
+    public Vector3 offset;
+	
+	void FixedUpdate () {
+
+        Vector3 nextPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, nextPosition, camSpeed);
+        transform.position = smoothPosition;
+
+        transform.LookAt(target);
+	}
+}
